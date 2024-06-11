cec8cf596b083d0c3049837add2091040dd7f24b
tsegaye27 <ttalegn29@gmail.com>
2024-06-11 16:50:44 +0300

    Setting up the project

---
+++ app/ceramics/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ceramics = () => {
+  return (
+    <div>
+      <h1>Ceramics</h1>
+    </div>
+  );
+};
+
+export default Ceramics;
---
+++ app/components/CeramicsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CeramicsList = () => {
+  return <div>CeramicsList</div>;
+};
+
+export default CeramicsList;
