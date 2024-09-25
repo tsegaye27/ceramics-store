439b3243a05eb106c6ac24574570ee5ebdf2b342
tsegaye27 <ttalegn29@gmail.com>
2024-09-25 21:05:30 +0300

    Refactor AuthContext and add AuthProvider to layout

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from "react";
+import { types } from "util";
+
+interface AuthContextType {
+  token: string | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(null);
+
+  const login = (newToken: string) => {
+    setToken(newToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
