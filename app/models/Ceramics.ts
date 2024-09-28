1738730516bf9017448342d1554506c8d608171e
tsegaye27 <ttalegn29@gmail.com>
2024-09-28 13:39:50 +0300

    Refactor database connection and add Ceramics model

---
+++ app/models/Ceramics.ts
@@ -0,0 +1,52 @@
+import { Schema, model, Document } from "mongoose";
+
+interface ICeramics extends Document {
+  size: string;
+  type: string;
+  manufacturer: string;
+  piecesPerPacket: number;
+  code: string;
+  totalArea?: number;
+  totalPackets: number;
+  totalPiecesWithoutPacket: number;
+}
+
+const ceramicsSchema = new Schema<ICeramics>({
+  size: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  piecesPerPacket: {
+    type: Number,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  totalArea: {
+    type: Number,
+    required: false,
+  },
+  totalPackets: {
+    type: Number,
+    required: true,
+  },
+  totalPiecesWithoutPacket: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Ceramics = model<ICeramics>("Ceramics", ceramicsSchema);
+
+export default Ceramics;
