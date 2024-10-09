089ea2d17c79dcf7ec0b40ad7e517d8daf6be7fb
tsegaye27 <ttalegn29@gmail.com>
2024-10-09 13:05:25 +0300

    Refactor Ceramics route to handle authentication with JWT

--- app/api/ceramics/route.ts
+++ app/api/ceramics/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/app/lib/mongodb";
+import jwt from "jsonwebtoken";
 import Ceramics, { ICeramics } from "@/app/models/Ceramics";
 
 export async function GET(request: Request) {
@@ -26,12 +27,27 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
   const body = await request.json();
+  const authHeader = request.headers.get("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
+    jwt.verify(token, process.env.JWT_SECRET as string);
+    console.log(body);
     const newCeramic = new Ceramics(body);
     await newCeramic.save();
     return NextResponse.json(newCeramic, { status: 201 });
   } catch (error: unknown) {
+    if (error instanceof jwt.JsonWebTokenError) {
+      return NextResponse.json(
+        { error: "Invalid or expired token" },
+        { status: 401 }
+      );
+    }
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
