ef190bb187cf19c7d6a07d2488f859cca396f881
tsegaye27 <ttalegn29@gmail.com>
2024-10-09 22:46:26 +0300

    Refactor User schema to include createdAt field

--- app/models/User.ts
+++ app/models/User.ts
@@ -3,19 +3,23 @@ import mongoose, { Schema, model, Document } from "mongoose";
 interface IUser extends Document {
   email: string;
   hashedPassword: string;
+  createdAt: Date;
 }
 
-const userSchema = new Schema<IUser>({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
   },
-  hashedPassword: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.models.User || model<IUser>("User", userSchema);
 
