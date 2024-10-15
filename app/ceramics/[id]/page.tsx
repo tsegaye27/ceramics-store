c52829fea71e6a853f96712aface5dca44579e66
tsegaye27 <ttalegn29@gmail.com>
2024-10-15 13:37:45 +0300

    Refactor CeramicDetail component: Add language switch buttons and translate text

--- app/ceramics/[id]/page.tsx
+++ app/ceramics/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import Link from "next/link";
+import { useLanguage } from "@/app/context/LanguageContext";
 
 interface ICeramic {
   _id: string;
@@ -21,6 +22,7 @@ interface ICeramic {
 export default function CeramicDetail() {
   const [ceramic, setCeramic] = useState<ICeramic | null>(null);
   const { id } = useParams();
+  const { t, switchLanguage } = useLanguage();
 
   useEffect(() => {
     const fetchCeramicById = async () => {
@@ -56,19 +58,27 @@ export default function CeramicDetail() {
 
   return (
     <div className="container mx-auto p-6 bg-blue-50 min-h-screen">
+      <button onClick={() => switchLanguage("en")} className="mr-2">
+        English
+      </button>
+      <button onClick={() => switchLanguage("am")} className="mr-2">
+        አማርኛ
+      </button>
       <div className="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-lg">
         <Link
           href="/ceramics"
           className="text-blue-500 hover:text-blue-700 mb-6 inline-block"
         >
-          Back
+          {t("back")}
         </Link>
         <h1 className="text-4xl font-extrabold mb-6 text-center text-blue-700">
-          Ceramic Details
+          {t("ceramicDetails")}
         </h1>
         <div className="space-y-4 text-blue-800">
           <div className="p-4 bg-blue-100 rounded-lg shadow-sm">
-            <strong className="block font-semibold text-blue-900">Date:</strong>
+            <strong className="block font-semibold text-blue-900">
+              {t("date")}:
+            </strong>
             <span>{formatDate(ceramic.createdAt)}</span>
           </div>
           <div className="p-4 bg-blue-100 rounded-lg shadow-sm">
