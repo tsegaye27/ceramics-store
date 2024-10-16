95e17f5b886b5a75082f7cf44cd8ee593e9cc9ca
tsegaye27 <ttalegn29@gmail.com>
2024-10-16 15:56:51 +0300

    Refactor layout component: Add ReduxProvider for state management

---
+++ app/ReduxProvider.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import store from "./store/store";
+import { Provider } from "react-redux";
+
+const ReactProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider store={store}>{children}</Provider>
+      </body>
+    </html>
+  );
+};
+
+export default ReactProvider;
