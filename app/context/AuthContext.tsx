a06f3e25c63262b50d959e176f2534bd6e237830
tsegaye27 <ttalegn29@gmail.com>
2024-10-23 19:26:59 +0300

    Refactor AuthContext: Add token expiration time and validation

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -7,40 +7,64 @@ interface AuthContextType {
   token: string | null;
   login: (token: string) => void;
   logout: () => void;
-  cookies: { token?: string };
+  cookies: { token?: string; expirationTime?: string };
   setCookie: (
-    name: "token",
+    name: string,
     value: string,
     options?: { path?: string; maxAge?: number; secure?: boolean }
   ) => void;
   removeCookie: (
-    name: "token",
+    name: "token" | "expirationTime",
     options?: { path?: string; maxAge?: number; secure?: boolean }
   ) => void;
+  isTokenValid: () => boolean;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [cookies, setCookie, removeCookie] = useCookies<
+  const [cookies, setCookie, removeCookie] = useCookies<string>([
     "token",
-    { token?: string }
-  >(["token"]);
+    "expirationTime",
+  ]);
+
   const token = cookies.token || null;
+  const expirationTime = cookies.expirationTime
+    ? Number(cookies.expirationTime)
+    : null;
 
   const login = (newToken: string) => {
+    const newExpirationTime = Date.now() + 60 * 60 * 24 * 1000; // 24 hours
     setCookie("token", newToken, {
       path: "/",
+      maxAge: 60 * 60 * 24,
+    });
+    setCookie("expirationTime", newExpirationTime.toString(), {
+      path: "/",
+      maxAge: 60 * 60 * 24,
     });
   };
 
   const logout = () => {
     removeCookie("token", { path: "/" });
+    removeCookie("expirationTime", { path: "/" });
+  };
+
+  const isTokenValid = () => {
+    return token && expirationTime && Date.now() < expirationTime;
   };
 
   return (
     <AuthContext.Provider
-      value={{ token, login, logout, cookies, setCookie, removeCookie }}
+      value={{
+        token,
+        login,
+        logout,
+        cookies,
+        setCookie,
+        removeCookie,
+        isTokenValid,
+      }}
     >
       {children}
     </AuthContext.Provider>
