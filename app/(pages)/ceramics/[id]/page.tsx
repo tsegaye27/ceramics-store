99290297b2061624dcd5eb4697724f4f2ce01619
tsegaye27 <ttalegn29@gmail.com>
2024-11-18 23:01:07 +0300

    refactor CeramicDetail component to use service for fetching ceramic data

--- app/(pages)/ceramics/[id]/page.tsx
+++ app/(pages)/ceramics/[id]/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { ICeramics } from "@/app/_models/ceramics/types";
 import { formatDate } from "@/app/_utils/helperFunctions";
-import { getCeramicById } from "@/app/_services/ceramicsService";
+import { serviceGetCeramicById } from "@/app/_services/ceramicsService";
 
 export default async function CeramicDetail({
   params,
 }: {
   params: { id: string };
 }) {
-  const ceramic: ICeramics | null = await getCeramicById(params.id);
+  const ceramic: ICeramics | null = await serviceGetCeramicById(params.id);
   return (
     <div className="container mx-auto p-6 bg-blue-50 min-h-screen">
       <div className="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-lg">
