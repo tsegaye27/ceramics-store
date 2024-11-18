0e914eb5edd9faad377f58cf37fa3b78e5d8e654
tsegaye27 <ttalegn29@gmail.com>
2024-11-19 01:24:20 +0300

    refactor CeramicForm component to use addNewCeramicAction for form submission; simplify input handling logic

--- app/(pages)/ceramics/add/page.tsx
+++ app/(pages)/ceramics/add/page.tsx
@@ -1,49 +1,7 @@
-import { ICeramics } from "@/app/_models/ceramics/types";
-import { serviceAddNewCeramic } from "@/app/_services/ceramicsService";
-import logger from "@/app/_utils/logger";
+import { addNewCeramicAction } from "@/app/_lib/actions";
 import Link from "next/link";
 
-const CeramicForm = () => {
-  const handleSubmit = async (formData: FormData) => {
-    "use server";
-    const size = formData.get("size") as string;
-    const manufacturer = formData.get("manufacturer") as string;
-    const code = formData.get("code") as string;
-    const piecesPerPacket = parseInt(
-      formData.get("piecesPerPacket") as string,
-      10
-    );
-    const totalPackets = parseInt(formData.get("totalPackets") as string, 10);
-    const totalPiecesWithoutPacket = parseInt(
-      formData.get("totalPiecesWithoutPacket") as string,
-      10
-    );
-    const ceramicType = formData.get("ceramicType") as string;
-
-    if (
-      !size ||
-      !manufacturer ||
-      !code ||
-      !ceramicType ||
-      !piecesPerPacket ||
-      !totalPackets ||
-      !totalPiecesWithoutPacket
-    ) {
-      throw new Error("Please fill all the fields");
-      // logger.warn("Please fill all the fields");
-    }
-
-    const ceramic: ICeramics = {
-      size,
-      manufacturer,
-      code,
-      piecesPerPacket,
-      totalPackets,
-      totalPiecesWithoutPacket,
-      type: ceramicType,
-    };
-    return await serviceAddNewCeramic(ceramic);
-  };
+const CeramicForm = async () => {
   return (
     <>
       <div className="p-6 max-w-md mx-auto bg-white rounded-lg shadow-md">
@@ -53,7 +11,7 @@ const CeramicForm = () => {
         <h1 className="text-3xl font-semibold text-center text-gray-800 mb-6">
           addNewCeramic
         </h1>
-        <form action={handleSubmit} className="flex flex-col space-y-4">
+        <form action={addNewCeramicAction} className="flex flex-col space-y-4">
           <div className="relative">
             <input
               type="text"
