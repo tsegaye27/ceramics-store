9baf133f25c91b3b4221a1c728566ca7473ffe16
tsegaye27 <ttalegn29@gmail.com>
2024-11-18 23:00:52 +0300

    refactor OrderList component to use service for fetching orders; simplify area calculation and improve rendering logic

--- app/(pages)/orders/page.tsx
+++ app/(pages)/orders/page.tsx
@@ -1,124 +1,63 @@
-"use client";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { IOrder } from "@/app/_models/orders/types";
+import { serviceGetOrders } from "@/app/_services/ordersService";
+import { calculateArea } from "@/app/_utils/helperFunctions";
 import Link from "next/link";
-import { useLanguage } from "../../_context/LanguageContext";
-// import Spinner from "../_components/Spinner";
 
-interface Order {
-  _id: string;
-  ceramicId: { code: string; size: string; piecesPerPacket: string };
-  userId: { name: string };
-  seller: string;
-  pieces: number;
-  packets: number;
-  price: number;
-  createdAt: string;
-}
-
-const OrderList = () => {
-  const [orders, setOrders] = useState<Order[]>([]);
-  const [isLoading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const { t, switchLanguage } = useLanguage();
-
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        setLoading(true);
-        const response = await axios.get("/api/orders");
-        setOrders(response.data);
-        setError(null);
-      } catch (err) {
-        setError("Failed to fetch sell orders. Please try again later.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchOrders();
-  }, []);
-
-  const calculateArea = (
-    totalPackets: number,
-    totalPiecesWithoutPacket: number,
+const OrderList = async () => {
+  const orders: IOrder[] = await serviceGetOrders();
+  const handleCalculateArea = (
+    packets: number,
+    pieces: number,
     piecesPerPacket: string,
     size: string
-  ) => {
-    const ppp = Number(piecesPerPacket);
-    if (size === "60x60") {
-      const area = totalPackets * ppp * 0.36 + totalPiecesWithoutPacket * 0.36;
-
-      return area.toFixed(2);
-    }
-    if (size === "30x60") {
-      const area = totalPackets * ppp * 0.18 + totalPiecesWithoutPacket * 0.18;
-
-      return area.toFixed(2);
-    }
-    if (size === "30x30") {
-      const area = totalPackets * ppp * 0.09 + totalPiecesWithoutPacket * 0.09;
-
-      return area.toFixed(2);
-    }
-    if (size === "40x40") {
-      const area = totalPackets * ppp * 0.16 + totalPiecesWithoutPacket * 0.16;
-
-      return area.toFixed(2);
-    }
-    return "0.00";
-  };
+  ) => calculateArea(packets, pieces, piecesPerPacket, size);
 
   return (
     <div className="container mx-auto p-4">
-      <button onClick={() => switchLanguage("en")} className="mr-2">
-        English
-      </button>
-      <button onClick={() => switchLanguage("am")} className="mr-2">
-        አማርኛ
-      </button>
-      <br />
       <Link
         href="/ceramics"
-        onClick={() => setLoading(true)}
         className="text-blue-500 hover:text-blue-700 mb-4 inline-block"
       >
-        {t("back")}
+        back
       </Link>
-      <h1 className="text-3xl font-bold mb-6 text-center">{t("orderList")}</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">orderList</h1>
       {orders.length === 0 ? (
-        <p className="text-center">{t("noOrdersYet")}</p>
+        <p className="text-center">noOrdersYet</p>
       ) : (
         <div className="overflow-x-auto">
           <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
             <thead>
               <tr className="bg-gray-100 text-left">
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("ceramic")}
+                  No
                 </th>
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("seller")}
+                  ceramic
                 </th>
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("time")}
+                  seller
                 </th>
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("totalArea")}
+                  time
                 </th>
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("totalPrice")}
+                  totalArea
                 </th>
                 <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  {t("user")}
+                  totalPrice
                 </th>
+                {/* <th className="py-3 px-4 border-b border-r-2 font-medium">
+                  user
+                </th> */}
               </tr>
             </thead>
             <tbody>
               {orders.map((order, index) => (
                 <tr
-                  key={order._id}
+                  key={order.ceramicId._id}
                   className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
                 >
+                  <td className="py-3 px-4 border-b border-r-2">{index + 1}</td>
                   <td className="py-3 px-4 border-b border-r-2">
                     {order.ceramicId.size}({order.ceramicId.code})
                   </td>
@@ -126,16 +65,17 @@ const OrderList = () => {
                     {order.seller}
                   </td>
                   <td className="py-3 px-4 border-b border-r-2">
-                    {new Date(order.createdAt).toLocaleString()}
+                    {order.createdAt &&
+                      new Date(order.createdAt).toLocaleString()}
                   </td>
                   <td className="py-3 px-4 border-b border-r-2">
-                    {calculateArea(
+                    {handleCalculateArea(
                       order.packets,
                       order.pieces,
                       order.ceramicId.piecesPerPacket,
                       order.ceramicId.size
                     )}{" "}
-                    {t("m")}²
+                    m²
                   </td>
                   <td className="py-3 px-4 border-b border-r-2">
                     {order.price *
@@ -147,11 +87,11 @@ const OrderList = () => {
                           order.ceramicId.size
                         )
                       )}{" "}
-                    {t("birr")}
+                    birr
                   </td>
-                  <td className="py-3 px-4 border-b border-r-2">
+                  {/* <td className="py-3 px-4 border-b border-r-2">
                     {order.userId.name}
-                  </td>
+                  </td> */}
                 </tr>
               ))}
             </tbody>
