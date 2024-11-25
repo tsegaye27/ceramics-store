655f29a0054da60760c0a2a9211dbcec21753e0d
tsegaye27 <ttalegn29@gmail.com>
2024-11-25 09:34:31 +0300

    refactor(ceramics): update CeramicsPage to use local state for search and improve internationalization

--- app/(pages)/ceramics/page.tsx
+++ app/(pages)/ceramics/page.tsx
@@ -1,56 +1,65 @@
-import { getCeramicsAction } from "@/app/_lib/actions";
-import { ICeramics } from "@/app/_models/ceramics/types";
+"use client";
+import { useLanguage } from "@/app/_context/LanguageContext";
+import { calculateArea } from "@/app/_utils/helperFunctions";
+import Link from "next/link";
 import {
   serviceGetAllCeramics,
   serviceSearchCeramics,
 } from "@/app/_services/ceramicsService";
-import { calculateArea } from "@/app/_utils/helperFunctions";
-import Link from "next/link";
+import { useEffect, useState } from "react";
+import { ICeramics } from "@/app/_models/ceramics/types";
 
-const CeramicsPage = async ({ searchParams }: CeramicsPageProps) => {
-  const searchQuery = searchParams?.search || "";
-  const ceramics = await getCeramicsAction(searchQuery);
+const CeramicsPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const { t } = useLanguage();
+  const [ceramics, setCeramics] = useState<ICeramics[]>([]);
+  useEffect(() => {
+    const fetchCeramics = async () => {
+      const ceram = searchQuery
+        ? await serviceSearchCeramics(searchQuery)
+        : await serviceGetAllCeramics();
+      setCeramics(ceram);
+    };
+    fetchCeramics();
+  }, [searchQuery]); 
+  
+  if(!ceramics.length) return <div>loading cearmics...</div>
 
   return (
     <div className="p-6 bg-blue-50 min-h-screen">
       <h1 className="text-4xl font-extrabold text-center text-blue-700 mb-8">
-        ceramicsList
+        {t("ceramicsList")}
       </h1>
 
       <div className="max-w-4xl mx-auto">
-        <div className="flex">
-          <form method="get" className="w-full">
-            <input
-              type="text"
-              name="search"
-              placeholder="searchCeramics"
-              defaultValue={searchQuery}
-              className="border border-blue-300 p-3 w-full mb-6 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <button
-              type="submit"
-              className="hidden"
-              aria-hidden="true"
-            ></button>
-          </form>
-        </div>
+        <form className="flex mb-6">
+          <input
+            type="text"
+            name="search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder={t("searchCeramics")}
+            className="border border-blue-300 p-3 w-full rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </form>
+
         <div className="w-4xl flex justify-between">
           <Link
             href="/ceramics/add"
             className="text-blue-600 hover:text-blue-800 mb-6 inline-block"
           >
-            addNewCeramic
+            {t("addNewCeramic")}
           </Link>
           <Link
             href="/orders"
             className="text-blue-600 hover:text-blue-800 mb-6 inline-block"
           >
-            orderList
+            {t("orderList")}
           </Link>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {ceramics.map((ceramic: ICeramics) => (
+          {ceramics.map((ceramic) => (
             <div
               key={ceramic._id}
               className={`bg-white p-5 rounded-lg ${
@@ -60,12 +69,16 @@ const CeramicsPage = async ({ searchParams }: CeramicsPageProps) => {
               } hover:transition-shadow duration-300`}
             >
               <h2 className="font-bold text-xl text-blue-800 mb-2">
-                code: {ceramic.code}
+                {t("code")}: {ceramic.code}
               </h2>
-              <p>size: {ceramic.size}</p>
-              <p>type: {ceramic.type}</p>
+              <p>
+                {t("size")}: {ceramic.size}
+              </p>
+              <p>
+                {t("type")}: {ceramic.type}
+              </p>
               <p className="mb-4">
-                totalArea:{" "}
+                {t("totalArea")}:{" "}
                 {calculateArea(
                   ceramic.totalPackets,
                   ceramic.totalPiecesWithoutPacket,
@@ -78,20 +91,20 @@ const CeramicsPage = async ({ searchParams }: CeramicsPageProps) => {
                 href={`/ceramics/${ceramic._id}`}
                 className="text-blue-500 hover:text-blue-600"
               >
-                viewDetails
+                {t("viewDetails")}
               </Link>
               <div className="flex space-x-4 mt-4">
                 <Link
                   href={`/ceramics/add/${ceramic._id}`}
                   className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition-colors duration-200"
                 >
-                  add
+                  {t("add")}
                 </Link>
                 <Link
                   href={`/ceramics/sell/${ceramic._id}`}
                   className="bg-white text-blue-600 border border-blue-600 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition-colors duration-200"
                 >
-                  sell
+                  {t("sell")}
                 </Link>
               </div>
             </div>
