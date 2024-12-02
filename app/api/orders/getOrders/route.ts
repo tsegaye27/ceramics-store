a9cbe8fe37fc4d76398d4560f3e1289aa08efef0
tsegaye27 <ttalegn29@gmail.com>
2024-12-02 21:26:42 +0300

    feat(api/orders): enhance GET orders endpoint to include specific fields in ceramicId population and improve error logging

--- app/api/orders/getOrders/route.ts
+++ app/api/orders/getOrders/route.ts
@@ -1,15 +1,21 @@
+import dbConnect from "@/app/_lib/mongoose";
 import { Order } from "@/app/_models/Orders";
 import logger from "@/app/_utils/logger";
-import { NextResponse, NextRequest } from "next/server";
-import dbConnect from "@/app/_lib/mongoose";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
     await dbConnect();
-    const orders = await Order.find().populate("ceramicId");
+
+    const orders = await Order.find().populate({
+      path: "ceramicId",
+      select: "size piecesPerPacket code",
+    });
+
     return NextResponse.json(orders, { status: 200 });
   } catch (error) {
-    logger.error(`${error}`);
+    logger.error(`Error in GET /api/orders/getOrders: ${error}`);
     return NextResponse.json(
       { message: "Internal server error" },
       { status: 500 }
