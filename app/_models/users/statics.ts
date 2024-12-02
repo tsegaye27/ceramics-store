89ffa219cd8212fbd3b2e68af14c10d95743991b
tsegaye27 <ttalegn29@gmail.com>
2024-12-02 09:22:19 +0300

    refactor(ceramics, orders, users): remove unused model and schema files

--- app/_models/users/statics.ts
+++
@@ -1,6 +0,0 @@
-// import { Model } from "mongoose";
-// import { IUser } from "./types";
-
-// export async function getUser(this: Model<IUser>) {
-//   return await this.find();
-// }
--- app/_models/users/types.ts
+++
@@ -1,9 +0,0 @@
-// import { Document } from "mongoose";
-
-// export interface IUser extends Document {
-//   name: string;
-//   email: string;
-//   hashedPassword: string;
-//   role: "chis" | "user";
-//   createdAt: Date;
-// }
