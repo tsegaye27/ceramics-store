b57b9ee0f3e81681297684367b1d32b1bad8d9bf
tsegaye27 <ttalegn29@gmail.com>
2024-12-02 21:25:31 +0300

    feat(auth): enhance loading component with spinner and improved styling

--- app/(pages)/(auth)/loading.tsx
+++ app/(pages)/(auth)/loading.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Loading() {
-  return <div>Loading...</div>;
-}
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="h-20 w-20 border-4 border-t-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;
