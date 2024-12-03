a17040b6f6891c0f72a59510e81d64d4d997f9fc
tsegaye27 <ttalegn29@gmail.com>
2024-12-03 10:11:57 +0300

    feat(ceramics/layout): replace ClientProviders with dynamic import of CeramicsLayout

--- app/(pages)/ceramics/layout.tsx
+++ app/(pages)/ceramics/layout.tsx
@@ -1,11 +1,9 @@
-import ClientProviders from "@/app/_layouts/ClientProvider";
+import dynamic from "next/dynamic";
+
+const CeramicsLayout = dynamic(() => import("@/app/_layouts/CeramcisLayout"), {
+  ssr: false,
+});
 
 export default function RLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <ClientProviders>{children}</ClientProviders>
-      </body>
-    </html>
-  );
+  return <CeramicsLayout>{children}</CeramicsLayout>;
 }
