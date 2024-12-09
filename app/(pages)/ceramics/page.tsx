3ad9ce6ca324b6880df5f5d0259442d3a3401050
tsegaye27 <ttalegn29@gmail.com>
2024-12-09 17:59:38 +0300

    refactor(ceramics): simplify fetch logic and enhance no ceramics found message

--- app/(pages)/ceramics/page.tsx
+++ app/(pages)/ceramics/page.tsx
@@ -3,7 +3,6 @@ import { useLanguage } from "@/app/_context/LanguageContext";
 import axiosInstance from "@/app/_lib/axios";
 import { ICeramic } from "@/app/_types/types";
 import { calculateArea } from "@/app/_utils/helperFunctions";
-import logger from "@/app/_utils/logger";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
@@ -21,15 +20,13 @@ const CeramicsPage = () => {
       setLoading(true);
       setError(null);
       try {
-        const res = debouncedSearchQuery
-          ? await axiosInstance.get("/ceramics/search", {
-              params: { search: debouncedSearchQuery },
-            })
-          : await axiosInstance.get("/ceramics/getAll");
+        const res = await axiosInstance.get("/ceramics/search", {
+          params: { search: debouncedSearchQuery },
+        });
+
         setCeramics(res.data.data || []);
-        logger.info("Ceramics fetched successfully", res.data);
       } catch (err) {
-        logger.error("Failed to fetch ceramics:", err);
+        console.error("Failed to fetch ceramics:", err);
         setError("Failed to load ceramics. Please try again.");
         setCeramics([]);
       } finally {
@@ -38,6 +35,8 @@ const CeramicsPage = () => {
     };
     fetchCeramics();
   }, [debouncedSearchQuery]);
+
+  const noCeramicsFound = ceramics && ceramics.length === 0;
 
   return (
     <div className="p-6 bg-blue-50 min-h-screen">
@@ -78,64 +77,65 @@ const CeramicsPage = () => {
           </div>
         ) : error ? (
           <p className="text-red-500 text-center">{error}</p>
-        ) : ceramics && ceramics.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {ceramics.map((ceramic) => (
-              <div
-                key={ceramic?._id}
-                className={`bg-white p-5 rounded-lg shadow-lg ${
-                  ceramic?.totalPackets <= 10 ? "border-2 border-red-600" : ""
-                } hover:transition-shadow duration-300`}
-              >
-                <h2 className="font-bold text-xl text-blue-800 mb-2">
-                  {t("code")}: {ceramic?.code || "N/A"}
-                </h2>
-                <p>
-                  {t("size")}: {ceramic?.size || "N/A"}
-                </p>
-                <p>
-                  {t("type")}: {ceramic?.type || "N/A"}
-                </p>
-                <p className="mb-4">
-                  {t("totalArea")}:{" "}
-                  {calculateArea(
-                    ceramic?.totalPackets || 0,
-                    ceramic?.totalPiecesWithoutPacket || 0,
-                    ceramic?.piecesPerPacket || 0,
-                    ceramic?.size || ""
-                  )}{" "}
-                  {ceramic?.size === "zekolo" ? "m" : "m²"}
-                </p>
-                <Link
-                  href={`/ceramics/${ceramic?._id}`}
-                  aria-label={`${t("viewDetails")} ${ceramic?.code}`}
-                  className="text-blue-500 hover:text-blue-600"
-                >
-                  {t("viewDetails")}
-                </Link>
-                <div className="flex space-x-4 mt-4">
-                  <Link
-                    href={`/ceramics/add/${ceramic?._id}`}
-                    aria-label={`${t("add")} ${ceramic?.code}`}
-                    className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition-colors duration-200"
-                  >
-                    {t("add")}
-                  </Link>
-                  <Link
-                    href={`/ceramics/sell/${ceramic?._id}`}
-                    aria-label={`${t("sell")} ${ceramic?.code}`}
-                    className="bg-white text-blue-600 border border-blue-600 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition-colors duration-200"
-                  >
-                    {t("sell")}
-                  </Link>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
+        ) : noCeramicsFound ? (
           <p className="col-span-3 text-center text-gray-500 mt-8">
             {t("noCeramicsFound")}
           </p>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {ceramics &&
+              ceramics.map((ceramic) => (
+                <div
+                  key={ceramic?._id}
+                  className={`bg-white p-5 rounded-lg shadow-lg ${
+                    ceramic?.totalPackets <= 10 ? "border-2 border-red-600" : ""
+                  } hover:transition-shadow duration-300`}
+                >
+                  <h2 className="font-bold text-xl text-blue-800 mb-2">
+                    {t("code")}: {ceramic?.code || "N/A"}
+                  </h2>
+                  <p>
+                    {t("size")}: {ceramic?.size || "N/A"}
+                  </p>
+                  <p>
+                    {t("type")}: {ceramic?.type || "N/A"}
+                  </p>
+                  <p className="mb-4">
+                    {t("totalArea")}:{" "}
+                    {calculateArea(
+                      ceramic?.totalPackets || 0,
+                      ceramic?.totalPiecesWithoutPacket || 0,
+                      ceramic?.piecesPerPacket || 0,
+                      ceramic?.size || ""
+                    )}{" "}
+                    {ceramic?.size === "zekolo" ? "m" : "m²"}
+                  </p>
+                  <Link
+                    href={`/ceramics/${ceramic?._id}`}
+                    aria-label={`${t("viewDetails")} ${ceramic?.code}`}
+                    className="text-blue-500 hover:text-blue-600"
+                  >
+                    {t("viewDetails")}
+                  </Link>
+                  <div className="flex space-x-4 mt-4">
+                    <Link
+                      href={`/ceramics/add/${ceramic?._id}`}
+                      aria-label={`${t("add")} ${ceramic?.code}`}
+                      className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500 transition-colors duration-200"
+                    >
+                      {t("add")}
+                    </Link>
+                    <Link
+                      href={`/ceramics/sell/${ceramic?._id}`}
+                      aria-label={`${t("sell")} ${ceramic?.code}`}
+                      className="bg-white text-blue-600 border border-blue-600 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition-colors duration-200"
+                    >
+                      {t("sell")}
+                    </Link>
+                  </div>
+                </div>
+              ))}
+          </div>
         )}
       </div>
     </div>
