0c6d443a4d3a075c2345eab534e34af9078f4436
tsegaye27 <ttalegn29@gmail.com>
2025-02-16 16:59:17 +0300

    feat(users): implement user slice with fetchUser thunk

---
+++ app/_features/users/slice.ts
@@ -0,0 +1,53 @@
+import axiosInstance from "@/app/_lib/axios";
+import { IUser } from "@/app/_types/types";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+type UserState = {
+  user: IUser;
+  loading: boolean;
+  error: string | null;
+};
+
+const initialState: UserState = {
+  user: {
+    id: "",
+    email: "",
+    name: "",
+    role: "",
+    createdAt: "",
+    updatedAt: "",
+  },
+  loading: false,
+  error: null,
+};
+
+export const fetchUser = createAsyncThunk(
+  "users/fetchUser",
+  async (userId: string) => {
+    const response = await axiosInstance.get(`/users/${userId}`);
+    return response.data;
+  },
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.data;
+      })
+      .addCase(fetchUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Failed to fetch user";
+      });
+  },
+});
+
+export default userSlice.reducer;
