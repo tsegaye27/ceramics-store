4e31bd241f80c1157e96c7d0f6a402283dbca940
tsegaye27 <ttalegn29@gmail.com>
2025-02-19 15:50:09 +0300

    feat(ceramicSchema): add CeramicFormData type and improve validations

--- app/_validators/ceramicSchema.ts
+++ app/_validators/ceramicSchema.ts
@@ -1,4 +1,3 @@
-import { ICeramic } from "@/app/_types/types";
 import { z } from "zod";
 
 const ceramicId = z.string().uuid("Invalid ceramic ID");
@@ -10,18 +9,15 @@ const piecesAdded = z
   .number()
   .int()
   .positive("Pieces added must be a positive integer");
-const totalPackets = z
+const totalPackets = z.coerce
   .number()
-  .int()
-  .positive("Total packets must be a positive integer");
-const totalPiecesWithoutPacket = z
+  .min(0, "Total packets cannot be negative");
+const totalPiecesWithoutPacket = z.coerce
   .number()
-  .int()
-  .positive("Total pieces without packet must be a positive integer");
-const piecesPerPacket = z
+  .min(0, "Total pieces without packet cannot be negative");
+const piecesPerPacket = z.coerce
   .number()
-  .int()
-  .positive("Pieces per packet must be a positive integer");
+  .min(1, "Pieces per packet must be at least 1");
 const packetsSold = z
   .number()
   .int()
@@ -41,11 +37,11 @@ export const createCeramicSchema = z.object({
   totalPackets,
   totalPiecesWithoutPacket,
   piecesPerPacket,
-  imageUrl: z.string().optional(),
-  size: z.string(),
-  type: z.string(),
-  code: z.string(),
-  manufacturer: z.string(),
+  imageUrl: z.string().url("Invalid image URL").optional(),
+  size: z.string().min(1, "Size is Required"),
+  type: z.string().min(1, "Type is Required"),
+  code: z.string().min(1, "Code is Required"),
+  manufacturer: z.string().min(1, "Manufacturer is Required"),
 });
 
 export const soldCeramicSchema = z.object({
@@ -53,3 +49,5 @@ export const soldCeramicSchema = z.object({
   packetsSold,
   piecesSold,
 });
+
+export type CeramicFormData = z.infer<typeof createCeramicSchema>;
