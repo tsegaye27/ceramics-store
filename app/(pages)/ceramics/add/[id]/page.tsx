7b4cc88097e79d5b3634bdd05adf7ef4a4fd8c14
tsegaye27 <ttalegn29@gmail.com>
2025-02-24 10:22:01 +0300

    feat(ceramics): implement add ceramic functionality with form validation

--- app/(pages)/ceramics/add/[id]/page.tsx
+++ app/(pages)/ceramics/add/[id]/page.tsx
@@ -1,8 +1,19 @@
 "use client";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import axiosInstance from "@/app/_lib/axios";
+
+import { useTransition } from "react";
 import { useLanguage } from "@/app/_context/LanguageContext";
+import toast from "react-hot-toast";
+import {
+  RootState,
+  useAppDispatch,
+  useAppSelector,
+} from "@/app/_features/store/store";
+import { useRouter } from "next/navigation";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { updateCeramicSchema } from "@/app/_validators/ceramicSchema";
+import { updateCeramic } from "@/app/_features/ceramics/slice";
 
 type AddCeramicProps = {
   params: {
@@ -10,113 +21,117 @@ type AddCeramicProps = {
   };
 };
 
+type FormData = z.infer<typeof updateCeramicSchema>;
+
 export default function AddCeramic({ params }: AddCeramicProps) {
-  const [packetsToAdd, setPacketsToAdd] = useState("");
-  const [piecesToAdd, setPiecesToAdd] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
   const { t } = useLanguage();
-  async function addCeramic(
-    ceramicId: string,
-    packetsAdded: number,
-    piecesAdded: number
-  ) {
-    try {
-      const response = await axiosInstance.patch(`/ceramics/addById`, {
-        ceramicId,
-        packetsAdded,
-        piecesAdded,
-      });
-    } catch (error: any) {
-      throw new Error(error.response.data.error);
-    }
-  }
-  const handleAdd = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setErrorMessage("");
-    setSuccessMessage("");
+  const dispatch = useAppDispatch();
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const { loading } = useAppSelector((state: RootState) => state.ceramics);
 
-    const packetsAdded = parseInt(packetsToAdd);
-    const piecesAdded = parseInt(piecesToAdd);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(updateCeramicSchema),
+  });
 
-    if (isNaN(packetsAdded) || isNaN(piecesAdded)) {
-      setErrorMessage("Both fields must be valid numbers.");
-      return;
-    }
-
-    try {
-      await addCeramic(params.id, packetsAdded, piecesAdded);
-      setSuccessMessage("Ceramic updated successfully!");
-      setPacketsToAdd("");
-      setPiecesToAdd("");
-    } catch (error: any) {
-      setErrorMessage(error.message);
-    }
+  const handleBack = () => {
+    startTransition(() => {
+      router.push("/ceramics");
+    });
   };
 
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    const packets = isNaN(data.packetsAdded) ? 0 : data.packetsAdded;
+    const pieces = isNaN(data.piecesAdded) ? 0 : data.piecesAdded;
+
+    try {
+      const result = await dispatch(
+        updateCeramic({
+          ceramicId: params.id,
+          packetsAdded: packets,
+          piecesAdded: pieces,
+        }),
+      ).unwrap();
+      console.log("Result:", result);
+
+      if (result?.message) {
+        toast.success(result.message);
+      } else {
+        toast.error(result.message);
+      }
+      reset();
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
   return (
-    <div className="container mx-auto p-6 bg-gray-50 min-h-screen flex items-center justify-center">
+    <div className="container mx-auto p-6 bg-transparent min-h-screen flex items-center justify-center">
       <div className="max-w-lg w-full bg-white p-8 rounded-xl shadow-md">
-        <Link
-          href="/ceramics"
+        <button
+          onClick={handleBack}
           className="text-blue-500 hover:text-blue-700 mb-6 inline-block font-medium"
         >
           {t("back")}
-        </Link>
-        <h1 className="text-3xl font-semibold mb-6 text-center text-gray-800">
-          {t("addCeramic")}
-        </h1>
-        <div className="space-y-4">
-          {errorMessage && (
-            <div className="text-red-500 bg-red-100 border border-red-200 rounded-lg p-3 text-center">
-              {errorMessage}
+        </button>
+        {isPending ? (
+          <div className="flex items-center justify-center h-40">
+            <div className="h-16 w-16 border-8 border-t-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <>
+            <h1 className="text-3xl font-semibold mb-6 text-center text-gray-800">
+              {t("addCeramic")}
+            </h1>
+            <div className="space-y-4">
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                <div>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    {t("packetsToAdd")}
+                  </label>
+                  <input
+                    type="number"
+                    {...register("packetsAdded", { valueAsNumber: true })}
+                    className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder={t("enterPackets")}
+                  />
+                  {errors.packetsAdded && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.packetsAdded.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    {t("piecesToAdd")}
+                  </label>
+                  <input
+                    type="number"
+                    {...register("piecesAdded", { valueAsNumber: true })}
+                    className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder={t("enterPieces")}
+                  />
+                  {errors.piecesAdded && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.piecesAdded.message}
+                    </p>
+                  )}
+                </div>
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className={`w-full py-3 rounded-lg font-semibold text-white transition duration-300 bg-blue-600 hover:bg-blue-700`}
+                >
+                  {loading ? t("updating") : t("add")}
+                </button>
+              </form>
             </div>
-          )}
-          {successMessage && (
-            <div className="text-green-500 bg-green-100 border border-green-200 rounded-lg p-3 text-center">
-              {successMessage}
-            </div>
-          )}
-          <form onSubmit={handleAdd} className="space-y-6">
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                {t("packetsToAdd")}
-              </label>
-              <input
-                type="text"
-                name="packetsToAdd"
-                value={packetsToAdd}
-                onChange={(e) => setPacketsToAdd(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                placeholder={t("enterPackets")}
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700 mb-2">
-                {t("piecesToAdd")}
-              </label>
-              <input
-                type="text"
-                name="piecesToAdd"
-                value={piecesToAdd}
-                onChange={(e) => setPiecesToAdd(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                placeholder={t("enterPieces")}
-              />
-            </div>
-            <button
-              type="submit"
-              disabled={!packetsToAdd || !piecesToAdd}
-              className={`w-full py-3 rounded-lg font-semibold text-white transition duration-300 ${
-                packetsToAdd && piecesToAdd
-                  ? "bg-blue-600 hover:bg-blue-700"
-                  : "bg-gray-300 cursor-not-allowed"
-              }`}
-            >
-              {t("add")}
-            </button>
-          </form>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
