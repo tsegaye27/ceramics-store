9391f53b2404c257f369f84687ca90b09d4ea8ab
tsegaye27 <ttalegn29@gmail.com>
2025-02-24 10:19:04 +0300

    feat(ceramics): fetch ceramic details using Redux and display loading state

--- app/(pages)/ceramics/[id]/page.tsx
+++ app/(pages)/ceramics/[id]/page.tsx
@@ -1,47 +1,55 @@
 "use client";
-import Link from "next/link";
 import { formatDate } from "@/app/_utils/helperFunctions";
-import { useEffect, useState } from "react";
-import axiosInstance from "@/app/_lib/axios";
+import { useEffect, useState, useTransition } from "react";
 import { ICeramic } from "@/app/_types/types";
 import { useLanguage } from "@/app/_context/LanguageContext";
 import Image from "next/image";
+import { useAppDispatch } from "@/app/_features/store/store";
+import toast from "react-hot-toast";
+import { ceramicDetails } from "@/app/_features/ceramics/slice";
+import { useRouter } from "next/navigation";
 
 export default function CeramicDetail({ params }: { params: { id: string } }) {
   const languageContext = useLanguage();
   const t = languageContext?.t;
   const [ceramic, setCeramic] = useState<ICeramic | null>(null);
+  const dispatch = useAppDispatch();
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
+  const handleBack = () => {
+    startTransition(() => {
+      router.push("/ceramics");
+    });
+  };
   useEffect(() => {
     const fetchDetails = async () => {
       try {
-        const res = await axiosInstance.get(
-          `/ceramics/getById?ceramicId=${params.id}`
-        );
-        setCeramic(res.data.data);
+        const response = await dispatch(ceramicDetails(params.id));
+        setCeramic(response.payload.data);
       } catch (err: any) {
-        console.error("Failed to fetch ceramic details", err);
+        toast.error("Failed to fetch ceramic details", err);
       }
     };
     fetchDetails();
   }, [params.id]);
 
-  if (!ceramic)
+  if (!ceramic || isPending)
     return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-50">
-        <div className="h-20 w-20 border-4 border-t-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+      <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+        <div className="h-16 w-16 border-8 border-t-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
     );
 
   return (
     <div className="container mx-auto p-6 bg-blue-50 min-h-screen">
       <div className="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-lg">
-        <Link
-          href="/ceramics"
+        <button
+          onClick={handleBack}
           className="text-blue-500 hover:text-blue-700 mb-6 inline-block"
         >
           {t("back")}
-        </Link>
+        </button>
         <h1 className="text-4xl font-extrabold mb-6 text-center text-blue-700">
           {t("ceramicDetails")}
         </h1>
@@ -54,7 +62,7 @@ export default function CeramicDetail({ params }: { params: { id: string } }) {
               width={400}
               height={400}
               priority
-              className="rounded-lg object-cover mx-auto shadow-md"
+              className="rounded-lg w-auto h-auto object-cover mx-auto shadow-md"
             />
           </div>
         )}
