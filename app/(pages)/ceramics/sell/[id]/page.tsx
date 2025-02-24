a1f7e5aab6bb4a8cdd9421ea83b581639d318a36
tsegaye27 <ttalegn29@gmail.com>
2025-02-24 18:22:18 +0300

    refactor(ceramics): Use react-hook-form for selling ceramic

--- app/(pages)/ceramics/sell/[id]/page.tsx
+++ app/(pages)/ceramics/sell/[id]/page.tsx
@@ -1,9 +1,19 @@
 "use client";
-import { useState } from "react";
-import Link from "next/link";
-import axiosInstance from "@/app/_lib/axios";
-import { useAuth } from "@/app/_context/AuthContext";
+
 import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { soldCeramicSchema } from "@/app/_validators/ceramicSchema";
+import { useLanguage } from "@/app/_context/LanguageContext";
+import {
+  RootState,
+  useAppDispatch,
+  useAppSelector,
+} from "@/app/_features/store/store";
+import { useTransition } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { sellCeramic } from "@/app/_features/ceramics/slice";
+import toast from "react-hot-toast";
 
 type SellCeramicProps = {
   params: {
@@ -11,155 +21,151 @@ type SellCeramicProps = {
   };
 };
 
+type FormData = z.infer<typeof soldCeramicSchema>;
+
 export default function SellCeramic({ params }: SellCeramicProps) {
-  const [packetsToSell, setPacketsToSell] = useState("");
-  const [piecesToSell, setPiecesToSell] = useState("");
-  const [pricePerArea, setPricePerArea] = useState("");
-  const [seller, setSeller] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
-  const { token } = useAuth();
+  const { t } = useLanguage();
+  const dispatch = useAppDispatch();
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { loading } = useAppSelector((state: RootState) => state.ceramics);
 
-  async function sellCeramic(
-    ceramicId: string,
-    packetsToSell: number,
-    piecesToSell: number,
-    pricePerArea: number,
-    seller: string
-  ) {
-    if (!token) return;
-    try {
-      await axiosInstance.post(
-        `/orders/createOrder`,
-        {
-          ceramicId,
-          packets: packetsToSell,
-          pieces: piecesToSell,
-          price: pricePerArea,
-          seller,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      await axiosInstance.patch(`/ceramics/sell`, {
-        ceramicId,
-        packetsSold: packetsToSell,
-        piecesSold: piecesToSell,
-      });
-    } catch (error: any) {
-      throw new Error(error.response.data.error);
-    }
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(soldCeramicSchema),
+  });
 
-  const handleSell = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setErrorMessage("");
-    setSuccessMessage("");
-
-    const packets = parseInt(packetsToSell, 10);
-    const pieces = parseInt(piecesToSell, 10);
-    const price = parseInt(pricePerArea, 10);
-
-    if (isNaN(packets) || isNaN(pieces) || isNaN(price) || !seller.trim()) {
-      setErrorMessage("All fields are required and must be valid.");
-      return;
-    }
-
-    try {
-      await sellCeramic(params.id, packets, pieces, price, seller);
-      setSuccessMessage("Ceramic sold successfully!");
-      setPacketsToSell("");
-      setPiecesToSell("");
-      setPricePerArea("");
-      setSeller("");
+  const handleBack = () => {
+    startTransition(() => {
       router.push("/ceramics");
-    } catch (error: any) {
-      setErrorMessage(error.message);
+    });
+  };
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    const packets = isNaN(data.packetsSold) ? 0 : data.packetsSold;
+    const pieces = isNaN(data.piecesSold) ? 0 : data.piecesSold;
+    const pricePerArea = isNaN(data.pricePerArea) ? 0 : data.pricePerArea;
+
+    try {
+      const result = await dispatch(
+        sellCeramic({
+          ceramicId: params.id,
+          packetsSold: packets,
+          piecesSold: pieces,
+          pricePerArea,
+          seller: data.seller,
+        }),
+      ).unwrap();
+      if (result?.message) {
+        toast.success("Ceramic sold successfully!");
+      } else {
+        toast.error("Failed to sell ceramic");
+      }
+      reset();
+    } catch (err: any) {
+      toast.error(err || "Failed to sell ceramic");
     }
   };
 
   return (
     <div className="container mx-auto p-6 bg-gray-100 min-h-screen">
       <div className="max-w-xl mx-auto bg-white p-8 rounded-lg shadow-lg">
-        <Link
-          href={`/ceramics`}
-          className="text-blue-600 hover:text-blue-800 mb-6 inline-block"
-        >
-          Back
-        </Link>
-        <h1 className="text-3xl font-semibold mb-6 text-center">
-          Sell Ceramic
-        </h1>
-        <div className="space-y-4">
-          {errorMessage && (
-            <div className="text-red-600 text-center">{errorMessage}</div>
-          )}
-          {successMessage && (
-            <div className="text-green-600 text-center">{successMessage}</div>
-          )}
-          <form onSubmit={handleSell}>
-            <div>
-              <label className="block font-medium mb-1">Packets to Sell:</label>
-              <input
-                type="text"
-                name="packetsToSell"
-                value={packetsToSell}
-                onChange={(e) => setPacketsToSell(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter packets"
-                required
-              />
-            </div>
-            <div>
-              <label className="block font-medium mb-1">Pieces to Sell:</label>
-              <input
-                type="text"
-                name="piecesToSell"
-                value={piecesToSell}
-                onChange={(e) => setPiecesToSell(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter pieces"
-                required
-              />
-            </div>
-            <div>
-              <label className="block font-medium mb-1">Price per Area:</label>
-              <input
-                type="text"
-                step="0.01"
-                name="pricePerArea"
-                value={pricePerArea}
-                onChange={(e) => setPricePerArea(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter price"
-                required
-              />
-            </div>
-            <div>
-              <label className="block font-medium mb-1">Seller:</label>
-              <input
-                type="text"
-                name="seller"
-                value={seller}
-                onChange={(e) => setSeller(e.target.value)}
-                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Enter seller name"
-                required
-              />
-            </div>
-
+        {isPending ? (
+          <div className="flex items-center justify-center h-40">
+            <div className="h-16 w-16 border-8 border-t-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        ) : (
+          <>
             <button
-              type="submit"
-              className="w-full bg-blue-600 text-white py-3 rounded-lg mt-4 hover:bg-blue-700 transition duration-300"
+              onClick={handleBack}
+              className="text-blue-600 hover:text-blue-800 mb-6 inline-block"
             >
-              Sell
+              {t("back")}
             </button>
-          </form>
-        </div>
+            <h1 className="text-3xl font-semibold mb-6 text-center">
+              {t("sellCeramic")}
+            </h1>
+            <div className="space-y-4">
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                <div>
+                  <label className="block text-sm font-medium text-gray-700 mb-2">
+                    {t("packetsToSell")}:
+                  </label>
+                  <input
+                    type="number"
+                    {...register("packetsSold", { valueAsNumber: true })}
+                    className="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder={t("enterPackets")}
+                  />
+                  {errors.packetsSold && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.packetsSold.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label className="block font-medium mb-1">
+                    {t("piecesToSell")}:
+                  </label>
+                  <input
+                    type="number"
+                    {...register("piecesSold", { valueAsNumber: true })}
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder={t("enterPieces")}
+                  />
+                  {errors.piecesSold && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.piecesSold.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label className="block font-medium mb-1">
+                    {t("pricePerArea")}:
+                  </label>
+                  <input
+                    type="number"
+                    {...register("pricePerArea", { valueAsNumber: true })}
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder={t("enterPrice")}
+                  />
+                  {errors.pricePerArea && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.pricePerArea.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label className="block font-medium mb-1">
+                    {t("seller")}:
+                  </label>
+                  <input
+                    type="text"
+                    {...register("seller")}
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    placeholder={t("enterSeller")}
+                  />
+                  {errors.seller && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.seller.message}
+                    </p>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full bg-blue-600 text-white py-3 rounded-lg mt-4 hover:bg-blue-700 transition duration-300"
+                >
+                  {loading ? t("selling...") : t("sell")}
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
