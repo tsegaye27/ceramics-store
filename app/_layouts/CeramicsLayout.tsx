0c33efc6f3305be54348a7d173ffe97ea79d376a
tsegaye27 <ttalegn29@gmail.com>
2025-02-25 19:07:39 +0300

    feat(ceramics): Implement loading state and login button

--- app/_layouts/CeramicsLayout.tsx
+++ app/_layouts/CeramicsLayout.tsx
@@ -2,8 +2,9 @@
 
 import LanguageSwitcher from "@/app/_components/LanguageSwitcher";
 import { useLanguage } from "../_context/LanguageContext";
-import Link from "next/link";
 import { useAuth } from "../_context/AuthContext";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
 
 export default function CeramicsLayout({
   children,
@@ -13,30 +14,44 @@ export default function CeramicsLayout({
   const { logout, token, user } = useAuth();
   const { t } = useLanguage();
   const name = user?.name;
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleLogin = () => {
+    startTransition(() => {
+      router.push("/login");
+    });
+  };
 
   return (
     <div className="p-6 bg-blue-50 min-h-screen">
-      <header className="flex justify-between items-center mb-6">
-        <LanguageSwitcher />
-        <div>
-          {token && user ? (
-            <button
-              onClick={logout}
-              className="text-red-500 hover:text-red-700 font-bold"
-            >
-              {name} - {t("logout")}
-            </button>
-          ) : (
-            <Link
-              href="/login"
-              className="text-blue-500 hover:text-blue-700 font-bold"
-            >
-              {t("login")}
-            </Link>
-          )}
-        </div>
-      </header>
-      <main>{children}</main>
+      {isPending ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <header className="flex justify-between items-center mb-6">
+            <LanguageSwitcher />
+            <div>
+              {token && user ? (
+                <button
+                  onClick={logout}
+                  className="text-red-500 hover:text-red-700 font-bold"
+                >
+                  {name} - {t("logout")}
+                </button>
+              ) : (
+                <button
+                  onClick={handleLogin}
+                  className="text-blue-500 hover:text-blue-700 font-bold"
+                >
+                  {t("login")}
+                </button>
+              )}
+            </div>
+          </header>
+          <main>{children}</main>
+        </>
+      )}
     </div>
   );
 }
