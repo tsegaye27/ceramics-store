fb1107b56a7e3d0e605fde38c4fd18f744280121
tsegaye27 <ttalegn29@gmail.com>
2025-02-25 11:35:46 +0300

    feat(orders): Fetch and display orders with search functionality

--- app/(pages)/orders/page.tsx
+++ app/(pages)/orders/page.tsx
@@ -1,45 +1,48 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { calculateArea } from "@/app/_utils/helperFunctions";
-import Link from "next/link";
-import axiosInstance from "@/app/_lib/axios";
+import { useEffect, useState, useTransition } from "react";
+import {
+  calculateArea,
+  calculateTotalPrice as totalPrice,
+  calculateTotalArea as totalArea,
+} from "@/app/_utils/helperFunctions";
 import { IOrder } from "@/app/_types/types";
 import { useLanguage } from "@/app/_context/LanguageContext";
-// import logger from "@/app/_utils/logger";
-// import Loading from "./loading";
+import { useAppDispatch } from "@/app/_features/store/store";
+import { useRouter } from "next/navigation";
+import { fetchOrders } from "@/app/_features/orders/slice";
+import toast from "react-hot-toast";
 
 const OrderList = () => {
   const [orders, setOrders] = useState<IOrder[]>([]);
   const [filteredOrders, setFilteredOrders] = useState<IOrder[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  // const [loading, setLoading] = useState<boolean>(true);
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [searchOrderQuery, setSearchOrderQuery] = useState<string>("");
   const { t } = useLanguage();
+  const dispatch = useAppDispatch();
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleBack = () => {
+    startTransition(() => {
+      router.back();
+    });
+  };
 
   useEffect(() => {
-    const fetchOrders = async () => {
+    async function fetchData() {
       try {
-        const res = await axiosInstance.get("/orders/getOrders");
-        if (Array.isArray(res.data)) {
-          setOrders(res.data);
-          setFilteredOrders(res.data);
-          // logger.info("Orders fetched successfully", res.data);
-        } else {
-          throw new Error("Unexpected API response format");
-        }
+        const result = await dispatch(fetchOrders()).unwrap();
+        setOrders(result);
+        setFilteredOrders(result);
       } catch (err: any) {
-        setError(err.message || "Failed to fetch orders");
-      } finally {
-        // setLoading(false);
+        toast.error(err.message);
       }
-    };
-
-    fetchOrders();
-  }, []);
+    }
+    fetchData();
+  }, [dispatch]);
 
   const handleSearch = (term: string) => {
-    setSearchTerm(term);
+    setSearchOrderQuery(term);
     const lowerCaseTerm = term.toLowerCase();
     const filtered = orders.filter((order) => {
       return (
@@ -51,150 +54,133 @@ const OrderList = () => {
     setFilteredOrders(filtered);
   };
 
-  const calculateTotalPrice = () => {
-    return filteredOrders.reduce((total, order) => {
-      const area: string | any = calculateArea(
-        order.packets,
-        order.pieces,
-        order.ceramicId?.piecesPerPacket ?? 0,
-        order.ceramicId?.size ?? 0,
-      );
-      return total + order.price * area;
-    }, 0);
-  };
-
-  const calculateTotalArea = () => {
-    return filteredOrders.reduce((total, orders) => {
-      const area: string | any = calculateArea(
-        orders.packets,
-        orders.pieces,
-        orders.ceramicId?.piecesPerPacket ?? 0,
-        orders.ceramicId?.size ?? 0,
-      );
-      return total + (Number(area) || 0);
-    }, 0);
-  };
-
-  // if (loading) {
-  //   return <Loading />;
-  // }
-
-  if (error) {
-    return <p className="text-center text-red-500">Error: {error}</p>;
-  }
+  const calculateTotalPrice = totalPrice(filteredOrders);
+  const calculateTotalArea = totalArea(filteredOrders);
 
   return (
     <div className="container mx-auto p-4">
-      <Link
-        href="/ceramics"
-        className="text-blue-500 hover:text-blue-700 mb-4 inline-block"
-      >
-        {t("back")}
-      </Link>
-      <h1 className="text-3xl font-bold mb-6 text-center">{t("orderList")}</h1>
-
-      {/* Search Bar */}
-      <div className="mb-4 flex justify-center">
-        <input
-          type="text"
-          placeholder={t("search") || "Search by seller, user, or ceramic code"}
-          value={searchTerm}
-          onChange={(e) => handleSearch(e.target.value)}
-          className="p-2 border border-gray-300 rounded-lg w-1/2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-
-      {filteredOrders.length === 0 ? (
-        <p className="text-center">{t("noOrdersFound")}</p>
+      {isPending ? (
+        <div>Loading...</div>
       ) : (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-            <thead>
-              <tr className="bg-gray-100 text-left">
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  No
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  Ceramic
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  Seller
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  Time
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  Total Area
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  Total Price
-                </th>
-                <th className="py-3 px-4 border-b border-r-2 font-medium">
-                  User
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredOrders.map((order, index) => (
-                <tr
-                  key={order.id}
-                  className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
-                >
-                  <td className="py-3 px-4 border-b border-r-2">{index + 1}</td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {order.ceramicId?.size} ({order.ceramicId?.code})
-                  </td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {order.seller}
-                  </td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {order.createdAt &&
-                      new Date(order.createdAt).toLocaleString()}
-                  </td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {calculateArea(
-                      order.packets,
-                      order.pieces,
-                      order.ceramicId?.piecesPerPacket ?? 0,
-                      order.ceramicId?.size ?? 0,
-                    )}{" "}
-                    m²
-                  </td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {(
-                      order.price *
-                      Number(
-                        calculateArea(
+        <>
+          <button
+            onClick={handleBack}
+            className="text-blue-500 hover:text-blue-700 mb-4 inline-block"
+          >
+            {t("back")}
+          </button>
+          <h1 className="text-3xl font-bold mb-6 text-center">
+            {t("orderList")}
+          </h1>
+
+          {/* Search Bar */}
+          <div className="mb-4 flex justify-center">
+            <input
+              type="text"
+              placeholder={
+                t("search") || "Search by seller, user, or ceramic code"
+              }
+              value={searchOrderQuery}
+              onChange={(e) => handleSearch(e.target.value)}
+              className="p-2 border border-gray-300 rounded-lg w-1/2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          {filteredOrders.length === 0 ? (
+            <p className="text-center">{t("noOrdersFound")}</p>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+                <thead>
+                  <tr className="bg-gray-100 text-left">
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      No
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      Ceramic
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      Seller
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      Time
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      Total Area
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      Total Price
+                    </th>
+                    <th className="py-3 px-4 border-b border-r-2 font-medium">
+                      User
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {filteredOrders.map((order, index) => (
+                    <tr
+                      key={order.id}
+                      className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                    >
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {index + 1}
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {order.ceramicId?.size} ({order.ceramicId?.code})
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {order.seller}
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {order.createdAt &&
+                          new Date(order.createdAt).toLocaleString()}
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {calculateArea(
                           order.packets,
                           order.pieces,
                           order.ceramicId?.piecesPerPacket ?? 0,
                           order.ceramicId?.size ?? 0,
-                        ),
-                      )
-                    ).toFixed(2)}{" "}
-                    birr
-                  </td>
-                  <td className="py-3 px-4 border-b border-r-2">
-                    {order.userId.name}
-                  </td>
-                </tr>
-              ))}
-              <tr className="bg-gray-200 font-bold">
-                <td colSpan={4} className="py-3 px-4 border-t text-right">
-                  Total:
-                </td>
-                <td className="py-3 px-4 border-t">
-                  {calculateTotalArea()} m2
-                </td>
+                        )}{" "}
+                        m²
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {(
+                          order.price *
+                          Number(
+                            calculateArea(
+                              order.packets,
+                              order.pieces,
+                              order.ceramicId?.piecesPerPacket ?? 0,
+                              order.ceramicId?.size ?? 0,
+                            ),
+                          )
+                        ).toFixed(2)}{" "}
+                        birr
+                      </td>
+                      <td className="py-3 px-4 border-b border-r-2">
+                        {order.userId.name}
+                      </td>
+                    </tr>
+                  ))}
+                  <tr className="bg-gray-200 font-bold">
+                    <td colSpan={4} className="py-3 px-4 border-t text-right">
+                      Total:
+                    </td>
+                    <td className="py-3 px-4 border-t">
+                      {calculateTotalArea} m2
+                    </td>
 
-                <td className="py-3 px-4 border-t">
-                  {calculateTotalPrice().toFixed(2)} birr
-                </td>
-                <td className="border-t"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                    <td className="py-3 px-4 border-t">
+                      {calculateTotalPrice.toFixed(2)} birr
+                    </td>
+                    <td className="border-t"></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
