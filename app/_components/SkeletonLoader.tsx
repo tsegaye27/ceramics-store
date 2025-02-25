a895b39e3d2a256956f76eb3560a0b5690d99558
tsegaye27 <ttalegn29@gmail.com>
2025-02-25 19:07:57 +0300

    feat: Implement SkeletonLoader component

---
+++ app/_components/SkeletonLoader.tsx
@@ -0,0 +1,18 @@
+const SkeletonLoader = () => (
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    {[...Array(6)].map((_, index) => (
+      <div
+        key={index}
+        className="bg-white p-5 rounded-lg shadow-lg animate-pulse"
+      >
+        <div className="h-48 bg-gray-300 rounded-t-lg mb-4"></div>
+        <div className="h-6 bg-gray-300 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 rounded mb-2"></div>
+      </div>
+    ))}
+  </div>
+);
+
+export default SkeletonLoader;
