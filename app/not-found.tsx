b22dad4615f3f1e58f41ec974c3131420b6c59d7
tsegaye27 <ttalegn29@gmail.com>
2025-02-28 14:59:57 +0300

    refactor: Redirect users based on role after login

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,8 @@
-import Link from "next/link";
-
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800">
       <h2 className="text-3xl font-bold mb-4">404 - Page Not Found</h2>
       <p className="mb-6">Sorry, the page you’re looking for doesn’t exist.</p>
-      <Link href="/">
-        <button className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-200">
-          Return Home
-        </button>
-      </Link>
     </div>
   );
 }
