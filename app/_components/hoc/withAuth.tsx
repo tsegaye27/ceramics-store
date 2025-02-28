529192da4aafe4802373ff939adbfef1029ac1c4
tsegaye27 <ttalegn29@gmail.com>
2025-02-28 14:58:57 +0300

    feat: Implement withAuth HOC for route protection

---
+++ app/_components/hoc/withAuth.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/app/_context/AuthContext";
+import { Loader } from "../Loader";
+
+const withAuth = (WrappedComponent: any, allowedRoles: string[] = []) => {
+  const Wrapper = (props: any) => {
+    const { user, loading } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!loading && !user) {
+        router.push("/login");
+      } else if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
+        router.push("/not-found");
+      }
+    }, [user, loading, router]);
+
+    if (loading || !user) return <Loader />;
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return Wrapper;
+};
+
+export default withAuth;
