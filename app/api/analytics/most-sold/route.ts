9d5c08f3681fa156ce8f3ccffc52de79169cd52b
tsegaye27 <ttalegn29@gmail.com>
2025-03-01 18:03:26 +0300

    feat(analytics): enhance most sold API with date range filtering

--- app/api/analytics/most-sold/route.ts
+++ app/api/analytics/most-sold/route.ts
@@ -3,7 +3,15 @@ import { decodeToken } from "../../_utils/decodeToken";
 import { checkPermission } from "../../_utils/checkPermission";
 import dbConnect from "../../_lib/mongoose";
 import { Order } from "../../_models";
-import { startOfToday, startOfWeek, startOfMonth } from "date-fns";
+import {
+  startOfToday,
+  startOfWeek,
+  startOfMonth,
+  endOfMonth,
+  endOfToday,
+  endOfWeek,
+  subWeeks,
+} from "date-fns";
 import logger from "@/services/logger";
 
 export async function GET(req: Request) {
@@ -18,14 +26,27 @@ export async function GET(req: Request) {
     const now = new Date();
 
     const dateRanges = {
-      today: startOfToday(),
-      thisWeek: startOfWeek(now, { weekStartsOn: 1 }),
-      thisMonth: startOfMonth(now),
+      today: {
+        start: startOfToday(),
+        end: endOfToday(),
+      },
+      thisWeek: {
+        start: startOfWeek(now, { weekStartsOn: 1 }),
+        end: endOfWeek(now, { weekStartsOn: 1 }),
+      },
+      thisMonth: {
+        start: subWeeks(startOfWeek(now, { weekStartsOn: 1 }), 3),
+        end: endOfWeek(now, { weekStartsOn: 1 }),
+      },
     };
 
-    const getMostSold = async (startDate: Date, limit: number) => {
+    const getMostSold = async (
+      startDate: Date,
+      endDate: Date,
+      limit: number,
+    ) => {
       return Order.aggregate([
-        { $match: { createdAt: { $gte: startDate } } },
+        { $match: { createdAt: { $gte: startDate, $lte: endDate } } },
         {
           $lookup: {
             from: "ceramics",
@@ -67,9 +88,9 @@ export async function GET(req: Request) {
     };
 
     const [today, thisWeek, thisMonth] = await Promise.all([
-      getMostSold(dateRanges.today, 5),
-      getMostSold(dateRanges.thisWeek, 1),
-      getMostSold(dateRanges.thisMonth, 1),
+      getMostSold(dateRanges.today.start, dateRanges.today.end, 5),
+      getMostSold(dateRanges.thisWeek.start, dateRanges.thisWeek.end, 1),
+      getMostSold(dateRanges.thisMonth.start, dateRanges.thisMonth.end, 1),
     ]);
     return successResponse(
       {
