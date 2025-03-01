2e32e6e4407b5a9211fd0989d9f3a60d6310c23b
tsegaye27 <ttalegn29@gmail.com>
2025-03-01 10:06:47 +0300

    feat: Implement dark mode support for loaders and skeletons

--- app/_components/SkeletonLoader.tsx
+++ app/_components/SkeletonLoader.tsx
@@ -3,13 +3,13 @@ const SkeletonLoader = () => (
     {[...Array(6)].map((_, index) => (
       <div
         key={index}
-        className="bg-white p-5 rounded-lg shadow-lg animate-pulse"
+        className="bg-white dark:bg-gray-800 p-5 rounded-lg shadow-lg animate-pulse"
       >
-        <div className="h-48 bg-gray-300 rounded-t-lg mb-4"></div>
-        <div className="h-6 bg-gray-300 rounded mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded mb-2"></div>
-        <div className="h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="h-48 bg-gray-300 dark:bg-gray-600 rounded-t-lg mb-4"></div>
+        <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 dark:bg-gray-600 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 dark:bg-gray-600 rounded mb-2"></div>
+        <div className="h-4 bg-gray-300 dark:bg-gray-600 rounded mb-2"></div>
       </div>
     ))}
   </div>
--- app/_components/hoc/withAuth.tsx
+++ app/_components/hoc/withAuth.tsx
@@ -5,11 +5,11 @@ import { Loader } from "../Loader";
 
 const withAuth = (WrappedComponent: any, allowedRoles: string[] = []) => {
   const Wrapper = (props: any) => {
-    const { user, loading } = useAuth();
+    const { token, user, loading } = useAuth();
     const router = useRouter();
 
     useEffect(() => {
-      if (!loading && !user) {
+      if (!user && !token) {
         router.push("/login");
       } else if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
         router.push("/not-found");
